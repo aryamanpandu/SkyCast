823ced188da4c02d5e25daaa3c19e3a78091324f
Aryaman Pandey <a.pandey@dal.ca>
2025-01-24 21:38:51 -0400

    Added initial work for SkyCast

--- a1/README.md
+++ a1/README.md
@@ -2,12 +2,18 @@
 
 ## Student Information
 
-- **Name**: [Enter your name here]
-- **Student ID**: [Enter your student ID here]
-- **Date Created**: [Enter the date you created this assignment]
+- **Name**: Aryaman Pandey
+- **Student ID**: B00925655
+- **Date Created**: 18 January 2025
 
 ## Application Type and Description
 
+I am going to be making a weather application.
+
+The website will contain data from the major cities, with the ability to search for weather for other applications. 
+
+
+I wanted to do this because I really enjoy watching the NBA and I would love to make a website to track data of top players in the league currently.
 Replace this text with actual description.
 
 ## Additional Details [As Needed]
@@ -16,4 +22,6 @@ If you do not have any additional details, please remove this section.
 
 ## Citations & Acknowledgements
 
-1. 
+1. Image of City: https://unsplash.com/photos/white-and-brown-city-buildings-during-daytime-Nyvq2juw4_o
+
+2. 
---
+++ a1/fetchLocation.js
@@ -0,0 +1,57 @@
+async function fetchLocation(search) {
+    try {
+            //https://geocoding-api.open-meteo.com/v1/search?name=Berlin&count=10&language=en&format=json
+        const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${search}&count=5`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const resultsArray = data.results;
+
+        if (resultsArray && Array.isArray(resultsArray)) {
+            resultsArray.forEach(element => {
+                console.log(element);
+            });
+        } else {
+            console.log("No results found or results is not an array.");
+        }
+    }
+    catch (error) {
+        console.log("Error fetching data: ", error);
+    }
+    
+
+}
+
+  //JSON response
+    // "results": [
+    // {
+    //   "id": 2950159,
+    //   "name": "Berlin",
+    //   "latitude": 52.52437,
+    //   "longitude": 13.41053,
+    //   "elevation": 74.0,
+    //   "feature_code": "PPLC",
+    //   "country_code": "DE",
+    //   "admin1_id": 2950157,
+    //   "admin2_id": 0,
+    //   "admin3_id": 6547383,
+    //   "admin4_id": 6547539,
+    //   "timezone": "Europe/Berlin",
+    //   "population": 3426354,
+    //   "postcodes": [
+    //     "10967",
+    //     "13347"
+    //   ],
+    //   "country_id": 2921044,
+    //   "country": "Deutschland",
+    //   "admin1": "Berlin",
+    //   "admin2": "",
+    //   "admin3": "Berlin, Stadt",
+    //   "admin4": "Berlin"
+    // },
+    // {
+    //   ...
+    // }]
